d7b4d7c59446cfff4daad23a9f8801a6017c6d17
Rea23 <rea.sunara@gmail.com>
2019-02-27 15:58:28 +0100

    updated project

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -7,56 +7,53 @@ import { FavBtn } from "../FavBtn/FavBtn";
 export class Details extends Component {
   constructor(props) {
     super(props);
-    let product = this.props.obj;
     this.state = {
       choosed: this.props.obj
     };
-  console.log(product);
-}
+  }
   render() {
     return (
       <div class="content">
-  <div class="content-header">
-    <Link to="/" id="back-link">Back</Link>
-    <h1>name</h1>
-    <div id="info-div">
-        <h3 id="product-price">{this.state.choosed.price}</h3>
-      <FavBtn id="product-fav" />
-    </div>
-  </div>
-  <div class="main-content">
-    <div class="img-div">
-      <img
-        id="img"
-        src="../../assets/images/{{ product.picture }}"
-        alt="image name"
-      />
-    </div>
-    <p>description</p>
-  </div>
-  {/* <Rate /> */}
-  <br />
-  <br />
-  <div>
-    <h2>Reviews</h2>
-    <div class="reviews-div">
-      <div class="reviews">
-        <div class="comm-rate-btns" id="rate-btn1">1</div>
-        <div class="comm-rate-btns" id="rate-btn2">2</div>
-        <div class="comm-rate-btns" id="rate-btn3">3</div>
-        <div class="comm-rate-btns" id="rate-btn4">4</div>
-        <div class="comm-rate-btns" id="rate-btn5">5</div>
-        <p>
-          <b>username:</b>
-          comment
-        </p>
+        <div class="content-header">
+          <Link to="/" id="back-link">
+            Back
+          </Link>
+          <h1>{this.state.choosed.name}</h1>
+          <div id="info-div">
+            <h3 id="product-price">${this.state.choosed.price}</h3>
+            <FavBtn id="product-fav" />
+          </div>
+        </div>
+        <div class="main-content">
+          <div class="img-div">
+            <img
+              id="img"
+              src={require("../../assets/images/" + this.state.choosed.picture)}
+              alt={this.state.choosed.name}
+            />
+          </div>
+          <p>{this.state.choosed.description}</p>
+        </div>
+        {/* <Rate /> */}
+        <br />
+        <br />
+        <div>
+          <h2>Reviews</h2>
+          <div class="reviews-div">
+            {this.state.choosed.reviews.map(Review => {
+              return (
+                  <p class="comment-div">
+                    <b>{Review.username} ({Review.rate}) &nbsp;</b>
+                    {Review.comment} 
+                    <br />
+                  </p>
+              );
+            })}
+          </div>
+        </div>
+        <br />
+        <br />
       </div>
-    </div>
-  </div>
-  <br />
-  <br />
-</div>
-
     );
   }
 }
